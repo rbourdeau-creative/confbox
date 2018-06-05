7c7f6da8c1f054bb09babb0c6115b61a83022238
Renaud Bourdeau <r.bourdeau@groupe-creative.fr>
2018-06-05 13:38:24 +0200

    initialize login form

--- src/Controllers/UsersController.php
+++ src/Controllers/UsersController.php
@@ -19,6 +19,19 @@ class UsersController extends Controller{
         }else{
             return $response->withRedirect('/404NotFound');
         }
+    }
 
+    public function login(RequestInterface $request, ResponseInterface $response){
+        if($request->getMethod() === "GET"){
+            return $this->render($response, 'users/login.twig');
+        }elseif ($request->getMethod() === "POST") {
+            $data = $request->getParsedBody();
+            echo "<pre>";
+            print_r($data);
+            echo "</pre>";
+            return true;
+        }else{
+            return $response->withRedirect('/404NotFound');
+        }
     }
 }
